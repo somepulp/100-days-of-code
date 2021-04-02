bb7fe60037df2dcdac31db51143e9efe703b1480
Joy Edem <koks.edem@gmail.com>
2021-04-02 14:57:02 -0400

    #3 removed pi module, added code for random number generation

--- beginner/day4/day4.py
+++ beginner/day4/day4.py
@@ -3,5 +3,11 @@ import random
 random_integer = random.randint(1, 50)
 print(random_integer)
 
-import pi_module
-print(pi_module.pi)
+#create a random floating point number between 0 and 1 
+print(random.random())
+
+#create a random floating point number between 0 and 5?
+random_float = random.random()
+print(random_float)
+#multiply by 5
+print(random_float*5)
