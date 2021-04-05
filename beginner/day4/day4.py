5fff39715cc6e1a04cf8c7cf2df24e945a80c6f9
Joy Edem <koks.edem@gmail.com>
2021-04-05 00:46:38 -0400

    closes #5 - added notes about lists, indexing, and methods: append and extend.

--- beginner/day4/day4.py
+++ beginner/day4/day4.py
@@ -1,13 +1,28 @@
-import random
+## Randomization
 
-random_integer = random.randint(1, 50)
-print(random_integer)
+# import random
 
-#create a random floating point number between 0 and 1 
-print(random.random())
+# random_integer = random.randint(1, 50)
+# print(random_integer)
 
-#create a random floating point number between 0 and 5?
-random_float = random.random()
-print(random_float)
-#multiply by 5
-print(random_float*5)
+# #create a random floating point number between 0 and 1 
+# print(random.random())
+
+# #create a random floating point number between 0 and 5?
+# random_float = random.random()
+# print(random_float)
+# #multiply by 5
+# print(random_float*5)
+
+## Lists
+states_of_america = ["Delaware", "Pennsylvania", "New York"]
+print(states_of_america[1])
+
+states_of_america[1] = "Alaska"
+print(states_of_america)
+
+states_of_america.append("Arkansas")
+print(states_of_america)
+
+states_of_america.extend(["Minnesota", "California"])
+print(states_of_america)
