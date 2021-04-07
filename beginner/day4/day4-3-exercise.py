6759115d94b63e399334c3ee834a545ce1c8ce6c
Joy Edem <koks.edem@gmail.com>
2021-04-06 22:58:40 -0400

    simplified code a bit

--- beginner/day4/day4-3-exercise.py
+++ beginner/day4/day4-3-exercise.py
@@ -12,10 +12,10 @@ map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 
-#solution
-two_digit = str(int(position))
-first_digit = int(two_digit[0])
-second_digit = int(two_digit[1])
+##Solution
+#conversions to remove whitespace
+first_digit = int(str(int(position))[0])
+second_digit = int(str(int(position))[1])
 if(first_digit < 1 or first_digit > len(row1) or second_digit<1 or second_digit>len(map)):
     print("Invalid coordinates. Please select co-ordinates that exist on the map.\nNote: the first box is at coordinate (1,1)")
 else:
