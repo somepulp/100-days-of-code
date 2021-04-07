4f0803940e91a03379a3e73d02122b0e15e6d373
Joy Edem <koks.edem@gmail.com>
2021-04-07 19:44:13 -0400

    closes #19 - added notes for the range operator used within the for loop

--- beginner/day5/day5.py
+++ beginner/day5/day5.py
@@ -4,3 +4,17 @@ fruits = ["Apple", "Peach", "Pear"]
 for fruit in fruits:
     print(fruit)
     print(fruit + " Pie")
+
+
+# For loops using a range
+for number in range(1, 10):
+    print(number)
+
+for number in range(11, 1, -2):
+    print(number)
+
+# Add up all numbers from 1 to 100:
+total = 0
+for number in range(1, 101):
+    total += number
+    print(total)
