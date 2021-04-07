9b10c213e9b02929dd4c11285ad7f427d54f1787
Joy Edem <koks.edem@gmail.com>
2021-04-06 22:53:36 -0400

    #8 - created solution for treasure map exercise. Added quick check for out of range coordinates

---
+++ beginner/day4/day4-3-exercise.py
@@ -0,0 +1,23 @@
+# Write a program that marks a spot with an 'X'
+# Using a two-digit system:
+# - first digit = vertical column number
+# - second digit = horizontal row number
+# Program will take the user input (a two digit number), and print out marked spot
+
+# starting code:
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+
+#solution
+two_digit = str(int(position))
+first_digit = int(two_digit[0])
+second_digit = int(two_digit[1])
+if(first_digit < 1 or first_digit > len(row1) or second_digit<1 or second_digit>len(map)):
+    print("Invalid coordinates. Please select co-ordinates that exist on the map.\nNote: the first box is at coordinate (1,1)")
+else:
+    map[second_digit-1][first_digit-1] = '❌'
+    print(f"{row1}\n{row2}\n{row3}")
