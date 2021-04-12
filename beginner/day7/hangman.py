eb104350f04c084dacdd16b173e754b210a18c70
Joy Edem <koks.edem@gmail.com>
2021-04-11 20:54:17 -0400

    created variable for word_length which was being used repeatedly

--- beginner/day7/hangman.py
+++ beginner/day7/hangman.py
@@ -14,7 +14,8 @@ print(f'Pssst, the solution is {chosen_word}.')
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 
 display = []
-for c in range(len(chosen_word)): 
+word_length = len(chosen_word)
+for c in range(word_length): 
     display += "_"
 
 # Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
@@ -23,7 +24,7 @@ guess = input("Guess a letter: ").lower()
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for i in range(len(chosen_word)):
+for i in range(word_length):
     if guess == chosen_word[i]:
         display[i] = guess
 
