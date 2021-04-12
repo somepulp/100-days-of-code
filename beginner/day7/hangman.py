28608ac2242d491c85ab0d7d70bfb6e586a7bec4
Joy Edem <koks.edem@gmail.com>
2021-04-11 21:29:27 -0400

    clean up file in preparation for next challenge

--- beginner/day7/hangman.py
+++ beginner/day7/hangman.py
@@ -1,9 +1,70 @@
 # Importing packages: 
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list) 
 word_length = len(chosen_word)
+
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+#Set 'lives' to equal 6.
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -13,11 +74,6 @@ display = []
 for c in range(word_length): 
     display += "_"
 
-#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has
-#          guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). 
-#          Then you can tell the user they've won.
-
-end_of_game = False
 while not end_of_game:
     # user guess, converted to lower case
     guess = input("Guess a letter: ").lower()
@@ -30,8 +86,17 @@ while not end_of_game:
             if guess == chosen_word[i]:
                 display[i] = guess
     
-    print(display)
+    #TODO-2: - If guess is not a letter in the chosen_word,
+    #Then reduce 'lives' by 1. 
+    #If lives goes down to 0 then the game should stop and it should print "You lose."
     
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+    
+    print(display)
+
     if "_" not in display:
         end_of_game = True
         print("You won!")
+    
+      #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
