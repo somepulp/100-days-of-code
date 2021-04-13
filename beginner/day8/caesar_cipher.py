8f7565e93695072dec7de160d1b7d44f65b11dd8
Joy Edem <koks.edem@gmail.com>
2021-04-13 18:45:37 -0400

    added starter code for final part

--- beginner/day8/caesar_cipher.py
+++ beginner/day8/caesar_cipher.py
@@ -36,3 +36,19 @@ def caesar(text, shift, direction):
 
 
 caesar(text,shift,direction)
+
+#TODO-1: Import and print the logo from art.py when the program starts.
+
+#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+#Try running the program and entering a shift number of 45.
+#Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
+#Hint: Think about how you can use the modulus (%).
