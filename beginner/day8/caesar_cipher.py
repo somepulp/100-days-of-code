62c77b57a3c6ecf36f12660b0816c3c0fd8ec4b3
Joy Edem <koks.edem@gmail.com>
2021-04-13 17:57:58 -0400

    added starter code for decryption

--- beginner/day8/caesar_cipher.py
+++ beginner/day8/caesar_cipher.py
@@ -26,3 +26,15 @@ def encrypt(text, shift):
      #🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
 encrypt("hello", 5)
+
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+  #e.g. 
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
+
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
