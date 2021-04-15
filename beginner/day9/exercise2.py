c53a0470445183829e3a7d8a671a235fbd177407
Joy Edem <koks.edem@gmail.com>
2021-04-14 23:53:36 -0400

    added starter code for #52

---
+++ beginner/day9/exercise2.py
@@ -0,0 +1,17 @@
+# Write a program that writes to a travel log. 
+# Create a function add_new_country that will add the entry for Russia to the travel_log: 
+# add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+
