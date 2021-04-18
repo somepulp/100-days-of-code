074f9c9ffdd9a032fc2c998e09c746369322cd70
Joy Edem <koks.edem@gmail.com>
2021-04-17 21:26:19 -0400

    changed customer data from a dictionary with two key-value pairs (name:..;bid:...} to a dictionary with one key-value pair: {name: $bid} - #53

--- beginner/day9/secret_auction/secret-auction-plan.md
+++ beginner/day9/secret_auction/secret-auction-plan.md
@@ -1,6 +1,6 @@
 1. Start -> Logo -> Welcome message
 2. Ask for Name
-3. Ask for Bid
+3. Ask for Bid -> add bid into dictionary
 4. Ask if other bidders\
 a. 'Yes' -> clear screen -> 2, 3, 4 \
 b. 'No' -> find highest bid, congratulate the bidder.\
--- beginner/day9/secret_auction/secret-auction.py
+++ beginner/day9/secret_auction/secret-auction.py
@@ -9,30 +9,35 @@ clear = lambda: os.system('clear')
 print(logo)
 print("Welcome to the secret auction program")
 more_bids = True
-customer_list = []
+customer_bids = {}
 
 while more_bids == True:
      name = input("What is your name?: ")
-     bid = input("What is your bid?: $")
-     more_bidders = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+     bid = float(input("What is your bid?: $"))
+     more_bidders = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
 
-     new_bid = {}
-     new_bid['customer_name'] = name
-     new_bid['bid_amount'] = bid
+     #new_bid = {}
+     customer_bids[name] = bid
+     #new_bid['bid_amount'] = bid
 
-     customer_list.append(new_bid)
+     #customer_list.appendl(new_bid)
 
      if more_bidders == 'no':
           more_bids = False
      else:
           clear()
 
-max_bid = customer_list[0]['bid_amount']
-bid_winner = customer_list[0]['customer_name']
-for item in customer_list:
-     if item['bid_amount'] > max_bid:
-          max_bid = item['bid_amount']
-          bid_winner = item['customer_name']
+#max_bid = customer_bids[0]['bid_amount']
+#bid_winner = customer_list[0]['customer_name']
+max_bid = 0
+bid_winner = ""
+
+for key in customer_bids:
+     if customer_bids[key] > max_bid:
+          max_bid = customer_bids[key]
+          bid_winner = key
+
+max_bid = round(max_bid,2)
 clear()
 
 print(f"The winner is {bid_winner} with a bid of ${max_bid}")
