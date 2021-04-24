f168ab074f3aad13070c316d778e17702ed758b9
Joy Edem <koks.edem@gmail.com>
2021-04-24 04:28:18 -0400

    added starter file and workflow for calculator app, created basic functions for operations #57

---
+++ beginner/day10/calculator/calculator.py
@@ -0,0 +1,23 @@
+# Calculator Program
+
+## Import packages and dependencies
+from art import logo
+
+# different functions of the calculator:
+
+#Add
+def add(n1, n2):
+     return n1 + n2
+
+#Subtract
+def subtract(n1, n2):
+     return n1 - n2
+
+#Multiply
+def multiply(n1, n2):
+     return n1 * n2
+
+#Divide
+def divide(n1, n2):
+     return n1 / n2
+
