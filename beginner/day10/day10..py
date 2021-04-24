954d2d5d33867e5318e2c19a2dba8dea22374ee2
Joy Edem <koks.edem@gmail.com>
2021-04-24 04:13:49 -0400

    #55 - added notes on docstrings (for documenting functions)

--- beginner/day10/day10..py
+++ beginner/day10/day10..py
@@ -1,5 +1,8 @@
 # take first name and last name to title case
 def format_name(fname, lname):
+     """ This function takes two string inputs representing the first
+     and last name, and returns the Title Case version of the name
+     """
      if fname == "" or lname == "":
           return 'Invalid inputs provided.'
      fullname = f"{fname} {lname}".title()
