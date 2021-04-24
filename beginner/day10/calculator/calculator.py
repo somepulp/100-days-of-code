3086cb20a1a988d99b959d5331f6432c5b950374
Joy Edem <koks.edem@gmail.com>
2021-04-24 04:39:21 -0400

    first draft of calculator program

--- beginner/day10/calculator/calculator.py
+++ beginner/day10/calculator/calculator.py
@@ -21,3 +21,26 @@ def multiply(n1, n2):
 def divide(n1, n2):
      return n1 / n2
 
+
+operations_dict = {
+     "+": add,
+     "-": subtract,
+     "*": multiply,
+     "/": divide,
+}
+
+num1 = int(input("What is the first number?: "))
+
+
+# loop through dictionary and print out keys (for operations)
+for key in operations_dict:
+     print(key)
+
+# ask user which operation they want to select
+operand = input("Pick an operation from the line above: ")
+num2 = int(input("What is the second number?: "))
+op_function = operations_dict[operand]
+
+answer = op_function(num1, num2)
+
+print(f"{num1} {operand} {num2} = {answer}")
