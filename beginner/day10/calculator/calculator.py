b672da382b2034a5a7b1029b5af26de7e84c280a
Joy Edem <koks.edem@gmail.com>
2021-04-25 20:44:24 -0400

    #57 - fixed while loop

--- beginner/day10/calculator/calculator.py
+++ beginner/day10/calculator/calculator.py
@@ -30,31 +30,19 @@ operations = {
 }
 
 num1 = int(input("What is the first number?: "))
-
-
 # loop through dictionary and print out keys (for operations)
 for key in operations:
      print(key)
-
-# ask user which operation they want to select
-operand = input("Pick an operation from the line above: ")
-num2 = int(input("What is the second number?: "))
-op_function = operations[operand]
-
-answer = op_function(num1, num2)
-
-print(f"{num1} {operand} {num2} = {answer}")
-
-again = input("Type 'y' to continue calculating with {answer}, or type 'n' to exit: ")
 continue_yes = True
 
 while continue_yes:
-     old_answer = answer
      operand = input("Pick an operation: ")
-     num3 = int(input("What is the next number?: "))
+     num2 = int(input("What is the next number?: "))
      op_function = operations[operand]
-     answer = op_function(old_answer, num3)
-     print(f"{old_answer} {operand} {num3} = {answer}")
+     answer = op_function(num1, num2)
+     print(f"{num1} {operand} {num2} = {answer}")
      again = input("Type 'y' to continue calculating with {answer}, or type 'n' to exit: ")
-     if again == 'n':
+     if again == 'y':
+          num1 = answer
+     else:
           continue_yes = False
