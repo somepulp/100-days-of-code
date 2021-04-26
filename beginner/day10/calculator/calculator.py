c23529828e34c458fdb7db18b35f4ea75dedf1da
Joy Edem <koks.edem@gmail.com>
2021-04-25 20:32:04 -0400

    while loop to continue program based on user input

--- beginner/day10/calculator/calculator.py
+++ beginner/day10/calculator/calculator.py
@@ -22,7 +22,7 @@ def divide(n1, n2):
      return n1 / n2
 
 
-operations_dict = {
+operations = {
      "+": add,
      "-": subtract,
      "*": multiply,
@@ -33,14 +33,28 @@ num1 = int(input("What is the first number?: "))
 
 
 # loop through dictionary and print out keys (for operations)
-for key in operations_dict:
+for key in operations:
      print(key)
 
 # ask user which operation they want to select
 operand = input("Pick an operation from the line above: ")
 num2 = int(input("What is the second number?: "))
-op_function = operations_dict[operand]
+op_function = operations[operand]
 
 answer = op_function(num1, num2)
 
 print(f"{num1} {operand} {num2} = {answer}")
+
+again = input("Type 'y' to continue calculating with {answer}, or type 'n' to exit: ")
+continue_yes = True
+
+while continue_yes:
+     old_answer = answer
+     operand = input("Pick an operation: ")
+     num3 = int(input("What is the next number?: "))
+     op_function = operations[operand]
+     answer = op_function(old_answer, num3)
+     print(f"{old_answer} {operand} {num3} = {answer}")
+     again = input("Type 'y' to continue calculating with {answer}, or type 'n' to exit: ")
+     if again == 'n':
+          continue_yes = False
