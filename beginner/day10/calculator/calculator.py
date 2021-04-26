497fa2781c624421a7cab180a74bfafd0a2ad00b
Joy Edem <koks.edem@gmail.com>
2021-04-25 20:51:45 -0400

    #57 finalized calculator program - used recursion to call the function within itself to restart the calculator based on user input

--- beginner/day10/calculator/calculator.py
+++ beginner/day10/calculator/calculator.py
@@ -4,7 +4,6 @@
 from art import logo
 
 # different functions of the calculator:
-
 #Add
 def add(n1, n2):
      return n1 + n2
@@ -29,20 +28,26 @@ operations = {
      "/": divide,
 }
 
-num1 = int(input("What is the first number?: "))
-# loop through dictionary and print out keys (for operations)
-for key in operations:
-     print(key)
-continue_yes = True
+def calculator():
+     print(logo)
 
-while continue_yes:
-     operand = input("Pick an operation: ")
-     num2 = int(input("What is the next number?: "))
-     op_function = operations[operand]
-     answer = op_function(num1, num2)
-     print(f"{num1} {operand} {num2} = {answer}")
-     again = input("Type 'y' to continue calculating with {answer}, or type 'n' to exit: ")
-     if again == 'y':
-          num1 = answer
-     else:
-          continue_yes = False
+     num1 = float(input("What is the first number?: "))
+     # loop through dictionary and print out keys (for operations)
+     for key in operations:
+          print(key)
+     continue_yes = True
+
+     while continue_yes:
+          operand = input("Pick an operation: ")
+          num2 = float(input("What is the next number?: "))
+          op_function = operations[operand]
+          answer = op_function(num1, num2)
+          print(f"{num1} {operand} {num2} = {answer}")
+          again = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+          if again == 'y':
+               num1 = answer
+          else:
+               continue_yes = False
+               calculator()
+
+calculator()
