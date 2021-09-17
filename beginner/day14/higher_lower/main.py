3fff525d4329121d7eb2a2c2913cff758bf0a727
Joy Edem <koks.edem@gmail.com>
2021-09-17 14:28:29 -0400

    finished higher-lower game

--- beginner/day14/higher_lower/main.py
+++ beginner/day14/higher_lower/main.py
@@ -12,47 +12,44 @@ def random_selections(lst, n):
 def user_correct(user_input, person_A_dict, person_B_dict):
     if user_input == "A":
         correct = person_A_dict["follower_count"] > person_B_dict["follower_count"]
+        dict = person_A_dict
     elif user_input == "B":
         correct = person_B_dict["follower_count"] > person_A_dict["follower_count"]
+        dict = person_B_dict
     else:
         correct = False
-    return(correct)
+    return( {
+        "correct": correct,
+        "selected": dict
+        })
 
 def higher_lower():
-    #TODO 1: print ascii art for higher lower
     print(logo)
-    #TODO 2: get random item from game_data - person_A - create random selector function
-    
+
     competitors = random_selections(lst = data, n = 2)
     person_A = competitors[0]
     person_B = competitors[1]
     correct_count = 0 
     game_continue = True
 
-    #TODO 3: extract name, description, country from random item
     while game_continue:
-        
+
         print(f'Compare A: {person_A["name"]}, a {person_A["description"]}, from {person_A["country"]}.')
-        
         print(vs)
-        #TODO 4: get another random item from game_data that is not same as previous item - person_B
-        # 
         print(f'Against B: {person_B["name"]}, a {person_B["description"]}, from {person_B["country"]}.')
-        #TODO 5: ask user who has more followers 'A' or 'B'
 
         user_input = input("Who has more followers? Type 'A' or 'B': ").capitalize()
-        
-        #TODO 6: check if user is right: if user_input = 'A', does person_A.followers > person_B.followers?
-        #if user_input = "B" does person_B.followers > person_A.followers? 
-        #TODO 6.5: create function that outputs true if user is right, false if user is wrong or they did not type A or B
-
 
         #TODO 7: if user is right, person A becomes the first person in competitor list. person b is another random person
-        if user_correct(user_input = user_input, person_A_dict = person_A, person_B_dict = person_B):
+        user_result = user_correct(user_input = user_input, person_A_dict = person_A, person_B_dict = person_B)
+        if user_result["correct"]:
             correct_count += 1
-            competitors[0] = competitors[1]
-            competitors[1] = random_selections(lst = data, n=1)
+            person_A = user_result["selected"]
+            person_B = random_selections(lst=data, n=1)
+            print(f"You're right! Current score: {correct_count}")
         else:
             game_continue = False
+            print(f"Wrong! Game over. Your final score was {correct_count}")
 
-#TODO 8: loop through - if user is right 
+
+higher_lower()
