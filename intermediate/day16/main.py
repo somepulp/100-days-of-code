260d8aeee42f60c52c09415a55cb6e49f1b1e87e
Joy Edem <koks.edem@gmail.com>
2021-10-18 22:29:13 -0400

    added starting code for oop coffee machine

--- intermediate/day16/main.py
+++ intermediate/day16/main.py
@@ -1,24 +1,3 @@
-# from turtle import Turtle, Screen
-
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape('turtle')
-# timmy.color('Seagreen4')
-
-# my_screen = Screen()
-# print(timmy.position())
-# timmy.forward(100)
-# print(timmy.position())
-
-# my_screen.exitonclick()
-
-from prettytable import PrettyTable
-from prettytable.prettytable import DOUBLE_BORDER
-
-table = PrettyTable()
-table.set_style(DOUBLE_BORDER)
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
-table.align = "l"
-
-print(table)
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
