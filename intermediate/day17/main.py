a3aaadfdc60c68733420590ecd40bb99fd29f554
Joy Edem <koks.edem@gmail.com>
2021-10-19 16:53:11 -0400

    added follow method for users

--- intermediate/day17/main.py
+++ intermediate/day17/main.py
@@ -5,8 +5,20 @@ class User:
         self.username = username
         print('new user has been created...')
         self.followers = 0 # default value - not necessary in the initialize step
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 user_1 = User("001", "joy")
 # every object from this User class must now have the id and username values
+user_2 = User("002", "gloria")
 
-print(user_1.id)
+user_1.follow(user_2)
+print(user_1.following)
+print(user_1.followers)
+print(user_2.following)
+print(user_2.followers)
+
+# print(user_1.id)
