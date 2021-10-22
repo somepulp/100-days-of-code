a7d1fdbdaa6973521eb12640d68488a9d40c6614
Joy Edem <koks.edem@gmail.com>
2021-10-22 13:55:41 -0400

    completed exercises for turtle class: creating square, dashed line, multi-shape diagram

---
+++ intermediate/day18/exercises.py
@@ -0,0 +1,58 @@
+from turtle import Turtle, Screen
+screen = Screen()
+
+timmy = Turtle()
+timmy.shape('turtle')
+timmy.color('forestgreen')
+timmy.pensize(2)
+# Draw a square
+timmy.forward(100)
+while round(timmy.ycor()) != 0 or round(timmy.xcor()) != 0:
+    timmy.right(90)
+    timmy.forward(100)
+
+timmy.reset()
+
+# Draw a dashed line
+counter = 0
+num_dashes = 20
+while counter != num_dashes:
+    timmy.pendown()    
+    timmy.forward(10)
+    timmy.penup()
+    timmy.forward(10)
+    counter +=1 
+
+timmy.reset()
+
+# Draw a beautiful multishape figure with random colors each time 
+import random 
+screen.colormode(255)
+screen.screensize()
+min_sides = 3 
+max_sides = 10
+FULL_DEGREE = 360
+timmy.pensize(2)
+
+def draw_shape(num_sides):
+    angle = FULL_DEGREE/num_sides
+    for i in range(num_sides):
+        timmy.right(angle)
+        timmy.forward(100)
+def random_color():
+    r = random.randrange(256)
+    g = random.randrange(256)
+    b = random.randrange(256)
+    random_color = (r,g,b)
+    return random_color
+
+while min_sides != max_sides:
+    new_color = random_color()
+    timmy.pencolor(new_color)
+    draw_shape(num_sides=min_sides) 
+    min_sides +=1   
+#while num_sides != 4:
+    
+
+
+screen.exitonclick()
