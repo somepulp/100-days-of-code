07f2ad734e2b828b25c73e1f1a902572230e7e6c
Joy Edem <koks.edem@gmail.com>
2021-11-02 19:05:39 -0400

    starter code for final day18 project

---
+++ intermediate/day18/main.py
@@ -0,0 +1,21 @@
+# import colorgram
+
+# colors = colorgram.extract("/home/joy/Documents/Python Learning/100-days-of-code/intermediate/day18/Ellipticine.png", 30)
+# rgb_colors = []
+
+# for _ in colors:
+#     rgb_colors.append(tuple(_.rgb))
+
+# print(colors)
+# print(rgb_colors)
+
+colors_list = [(34, 108, 167), (223, 229, 235), (227, 233, 230), (245, 77, 36), (112, 163, 211),
+                   (153, 57, 85), (219, 156, 94), (201, 60, 27), (24, 133, 55), (246, 204, 84), 
+                   (190, 151, 47), (225, 119, 152), (46, 53, 121), (221, 68, 97), (113, 199, 156), 
+                   (147, 37, 30), (253, 202, 0), (91, 113, 192), (74, 40, 32), (248, 153, 143), 
+                   (111, 41, 49), (155, 212, 203), (53, 174, 163), (38, 31, 67), (154, 210, 219), 
+                   (43, 33, 45), (35, 55, 46), (99, 93, 2)]
+
+# paint a painting with 10 by 10 rows of spots
+# dots 20 in size, spaced apart by 50 paces
+
