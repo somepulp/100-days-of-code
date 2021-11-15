27e26ed4f3785398a3c82c7ed262c582bb981205
Joy Edem <koks.edem@gmail.com>
2021-11-15 17:24:09 -0500

    pong game - created gameboard, paddles, event listener, and used while loop to update screen upon key presses

---
+++ intermediate/pong_game/main.py
@@ -0,0 +1,34 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+from scoreboard import GameBoard, Score
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Classic: Pong Game")
+screen.tracer(0)
+
+board = GameBoard()
+board.draw_lines()
+
+left_score = Score(-150, 225)
+right_score = Score(150, 225)
+
+right_paddle = Paddle(350)
+left_paddle = Paddle(-350)
+#screen.update()
+
+screen.listen()
+screen.onkey(fun=right_paddle.up, key="Up")
+screen.onkey(fun=right_paddle.down, key="Down")
+
+screen.onkey(fun=left_paddle.up, key="w")
+screen.onkey(fun=left_paddle.down, key="s")
+
+#screen.update()
+
+game_on = True
+while game_on:
+    screen.update()
+    
+screen.exitonclick()
