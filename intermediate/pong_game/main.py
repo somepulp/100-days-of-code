840c3010bfa018e34bbd9c630a7745db6a5a23ac
Joy Edem <koks.edem@gmail.com>
2021-11-18 12:48:34 -0500

    code to detect when ball hits wall or paddle, and if player scores on the other one.. still to figure out... move speed #TODO

--- intermediate/pong_game/main.py
+++ intermediate/pong_game/main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from paddle import Paddle
 from scoreboard import GameBoard, Score
 from ball import Ball
@@ -10,21 +10,20 @@ screen.bgcolor("black")
 screen.title("Classic: Pong Game")
 screen.tracer(0)
 
-board = GameBoard()
-board.draw_lines()
-
-left_score = Score(-150, 225)
-right_score = Score(150, 225)
-
 right_paddle = Paddle(350)
 left_paddle = Paddle(-350)
-#screen.update()
+board = GameBoard()
+#board.draw_lines()
+
+left_score = Score((-150, 225))
+right_score = Score((150, 225))
+
 ball = Ball()
+#screen.update()
 
 screen.listen()
 screen.onkey(fun=right_paddle.up, key="Up")
 screen.onkey(fun=right_paddle.down, key="Down")
-
 screen.onkey(fun=left_paddle.up, key="w")
 screen.onkey(fun=left_paddle.down, key="s")
 
@@ -32,8 +31,26 @@ screen.onkey(fun=left_paddle.down, key="s")
 
 game_on = True
 while game_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
     
+    # Detect when ball hits wall
+    if abs(ball.ycor()) > 280:
+        ball.bounce_y()
+    
+    # Detect when ball hits either paddle
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+        ball.move_speed *= 1.001
+         
+    # Detect when ball misses right paddle - reset but also reverse direction
+    if ball.xcor() > 370:
+        left_score.increase_score()
+        ball.reset_position()
+    
+    if ball.xcor() < -380:
+        right_score.increase_score()
+        ball.reset_position()
+        
 screen.exitonclick()
