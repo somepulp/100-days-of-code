47fe1ef01ebb3cc1ce0aa460af95ebee3a308b2b
Joy Edem <koks.edem@gmail.com>
2021-11-18 12:47:27 -0500

    increased paddle movement

--- intermediate/pong_game/paddle.py
+++ intermediate/pong_game/paddle.py
@@ -12,7 +12,7 @@ class Paddle(Turtle):
         self.shapesize(stretch_len=1, stretch_wid=5)
         
     def down(self):
-        self.goto(self.xcor(), self.ycor() - 10)
+        self.goto(self.xcor(), self.ycor() - 20)
         
     def up(self):
-        self.goto(self.xcor(), self.ycor() + 10)
+        self.goto(self.xcor(), self.ycor() + 20)
