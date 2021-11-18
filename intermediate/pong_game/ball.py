bdfd49231d76f834187a65d43b807b5252dc7c14
Joy Edem <koks.edem@gmail.com>
2021-11-18 12:47:01 -0500

    ball methods

--- intermediate/pong_game/ball.py
+++ intermediate/pong_game/ball.py
@@ -5,7 +5,23 @@ class Ball(Turtle):
         super().__init__(shape="circle")
         self.penup()
         self.color("white")
+        self.x_move = 1
+        self.y_move = 1
+        self.move_speed = 0.002
         
     
     def move(self):
-        self.goto(self.xcor()+10, self.ycor()+10)
+        self.goto(self.xcor() + self.x_move, self.ycor() + self.y_move)
+        
+    def bounce_y(self):
+        self.y_move *= -1 
+        
+    
+    def bounce_x(self):
+        self.x_move *= -1 
+        
+    def reset_position(self):
+        self.move_speed = 0.002
+        self.goto((0,0))
+        self.bounce_x()
+    
