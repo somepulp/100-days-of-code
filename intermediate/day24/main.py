99347054b1c5dbfeaf8e7a00f05f2bc34e23d890
Joy Edem <koks.edem@gmail.com>
2022-05-14 09:42:03 -0400

    - writing to a file, using mode = 'w' overwrites all  file contents

--- intermediate/day24/main.py
+++ intermediate/day24/main.py
@@ -1,3 +1,9 @@
+# Reading to a file
 with open("my_file.txt") as file:
   contents = file.read()
   print(contents)
+
+
+# Writing to a file 
+with open("my_file.txt", mode="w") as file:
+  file.write("Using mode = 'w' overwrites everything in the file with the new text")
