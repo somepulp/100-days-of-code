01158dbb79388a3639c835c8021e14492a8230d5
Joy Edem <koks.edem@gmail.com>
2022-05-14 12:18:55 -0400

    better code - using read instead of readlines and its possible to have an open statement within an open statement

--- intermediate/mail_merge/main.py
+++ intermediate/mail_merge/main.py
@@ -3,13 +3,14 @@ with open("./input/names/invited_names.txt") as n:
     names = n.readlines()
 names = [name.strip("\n") for name in names]
 
-# Get letter template
-with open("./input/letters/starting_letter.txt") as file:
-    starting_letter = file.readlines()
-starting_letter = [line.strip("\n") for line in starting_letter]
+PLACEHOLDER = "[name]"
 
-#Create and save a letter for each invited name 
-for name in names:
-    new_letter = [line.replace("[name]", name) + "\n" for line in starting_letter]
-    with open(f"./output/readytosend/letter_for_{name}.txt", 'w') as l:
-            l.writelines(new_letter)
+# Read letter template
+with open("./input/letters/starting_letter.txt") as s:
+    starting_letter = s.read()
+    #Create and save a letter for each invited name 
+    for name in names:
+        new_letter = starting_letter.replace(PLACEHOLDER, name)
+        with open(f"./output/readytosend/letter_for_{name}.txt", 'w') as l:
+                l.write(new_letter)
+
