e1e65e4c07fbd84fe307be074af63c3868af59fe
Joy Edem <koks.edem@gmail.com>
2022-05-14 10:15:45 -0400

    writing to a file that doesn't yet exist (using mode='w') causes the file to be created

--- intermediate/day24/main.py
+++ intermediate/day24/main.py
@@ -7,3 +7,8 @@ with open("my_file.txt") as file:
 # Writing to a file 
 with open("my_file.txt", mode="a") as file:
   file.write("\nUsing mode = 'a', the new text gets appended to the file, rather than replacing the old contents. Dont forget the newline character, if it's needed ('\\n') ")
+
+
+# Writing to a file that doesnt exist (using write mode)
+with open("new_file.txt", mode = 'w') as file:
+  file.write("This file didn't exist previously, and was created")
