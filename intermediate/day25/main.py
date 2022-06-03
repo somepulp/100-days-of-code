d33b8c4155423b277306e4d36b5a62c37eba2995
Joy Edem <koks.edem@gmail.com>
2022-06-02 20:25:51 -0400

    Working with tabular data, we can use the csv package, but its not as convenient as pandas

---
+++ intermediate/day25/main.py
@@ -0,0 +1,14 @@
+# Working with Tabular Data
+
+# import csv
+
+# with open("weather_data.csv") as file:
+#   data = csv.reader(file)
+#   print(data)
+#   temperatures = []
+#   for row in data:
+#     if data.line_num != 1:
+#       temperatures.append(int(row[1]))
+      
+# print(temperatures)
+
