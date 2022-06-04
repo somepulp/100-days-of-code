af3e0e7be7cc3bcb79dafcf69cf1fae74ae0733b
Joy Edem <koks.edem@gmail.com>
2022-06-04 19:59:49 -0400

    created state guessing gamewith turtle methods: write, setpos, textinput

--- intermediate/day25/us-states-game-start/main.py
+++ intermediate/day25/us-states-game-start/main.py
@@ -11,4 +11,22 @@ turtle.shape(image)
 states_data = pd.read_csv("./50_states.csv")
 states = states_data["state"].to_list()
 
+num_correct = 0 
+
+
+while num_correct < 50: 
+
+    answer_state = screen.textinput(title=f"{str(num_correct)}/50 States Correct", 
+                                    prompt="What's another state's name?").title()
+    if answer_state in states: 
+        num_correct += 1
+        states.remove(answer_state)
+        answer_turtle = turtle.Turtle()
+        answer_turtle.hideturtle()
+        answer_turtle.penup()
+        answer_row = states_data[states_data.state == answer_state]
+        answer_turtle.setpos(answer_row.x.item(), answer_row.y.item())
+        answer_turtle.write(answer_state, move=True,font=('Arial', 6, 'normal'),align='Center')
+    
+
 screen.exitonclick()
